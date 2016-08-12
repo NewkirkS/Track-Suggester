927cf8178c0ec913c1096ee309888f61cfc3e2e1
Stephen Newkirk <newkirk771@gmail.com>
2016-08-12 11:52:09 -0700

    Clean up spacing in html and add beginning of back end JS. Create beginning state variables for results and highScore variable for selecting highest value results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+// ===BACK=END===
+var javaScore = 0;
+var c#Score = 0;
+var rubyScore = 0;
+var phpScore = 0;
+var designScore = 0;
 
+var highScore = Math.max(javaScore, c#Score, rubyScore, phpScore, designScore);
 
+// ===FRONT=END===
 
 $(document).ready(function() {
   $('#output').modal({
